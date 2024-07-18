5cb7668e94a501e599d9a533df8141e16e86fea2
Joshua Sotto <joshuasottoelyen@gmail.com>
2024-07-19 04:36:41 +0800

    testing

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,9 @@ const UserController = require('../controllers/user.controller')
 const AuthMiddleware = require('../middlewares/auth.middleware')
 
 router.get('', AuthMiddleware, UserController.getAll)
+router.get('/testing', (req, res) => {
+    return res.send("testing")
+})
 router.post('/sign-up', UserController.signUp)
 router.post('/login', UserController.login)
 router.post('/:userId/messages', AuthMiddleware, UserController.sendMessage)
